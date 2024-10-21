c317e4ba4121242245725feea8511e40b590f9aa
林明虎 <hayashi20030703@gmail.com>
2024-10-21 23:06:22 +0900

    sharedPreferencesのsingletonを作成した

---
+++ riverpod_practice/lib/model/external/pref_service.dart
@@ -0,0 +1,20 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+PrefService prefService_1 = PrefService();
+PrefService prefService_2 = PrefService();
+
+class PrefService {
+  static final PrefService _instance = PrefService._internal();
+  static SharedPreferences? _pref;
+
+  PrefService._internal();
+
+  factory PrefService() {
+    return _instance;
+  }
+
+  Future<SharedPreferences> get pref async {
+    _pref ??= await SharedPreferences.getInstance();
+    return _pref!;
+  }
+}
--- riverpod_practice/lib/model/rp_admin_user.dart
+++ riverpod_practice/lib/model/rp_admin_user.dart
@@ -1,6 +1,10 @@
+import 'package:riverpod_practice/model/external/pref_service.dart';
 import 'package:riverpod_practice/model/rp_user.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'dart:convert';
 
 // クラスの定義
 class RPAdminUser extends RPUser {
@@ -9,6 +13,22 @@ class RPAdminUser extends RPUser {
     required super.name,
     required super.age,
   });
+
+  // JSONからRPAdminUserを生成するファクトリーメソッド
+  factory RPAdminUser.fromJson(Map<String, dynamic> json) {
+    return RPAdminUser(
+      name: json['name'],
+      age: json['age'],
+    );
+  }
+
+  // RPAdminUserをJSONに変換するメソッド
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'age': age,
+    };
+  }
 }
 
 // Provider
@@ -23,10 +43,26 @@ final rpAdminUserProvider =
 class RPUserNotifier extends StateNotifier<
     RPAdminUser> /* StateNotifierの型パラメータは、監視するstateの型（RPUser）を指定 */ {
   RPUserNotifier()
-      : super(RPAdminUser(name: '', age: 20)) /* 初期stateをRPUserで指定 */;
+      : super(RPAdminUser(name: '', age: 20)) /* 初期stateをRPUserで指定 */ {
+    // sharedPreferenceからデータを取得
+    PrefService().pref.then((pref) {
+      final savedAdminUser = pref.getString("rpAdminUser");
+      if (savedAdminUser != null) {
+        final readAdminUser = RPAdminUser.fromJson(json.decode(pref.getString(
+            "rpAdminUser")!)); // sharedPreferenceから取得したデータをRPAdminUserに変換
+        state = readAdminUser; // stateを更新
+      }
+    });
+  }
 
   // adminUser状態を操作するメソッド
   void updateAdminUser({required RPAdminUser newAdminUser}) {
     state = newAdminUser;
+    PrefService.pref.then((pref) {
+      pref.setString("rpAdminUser", json.encode(newAdminUser.toJson()));
+    });
+    PrefService.pref.then((pref) {
+      pref.setString("rpAdminUser", json.encode(newAdminUser.toJson()));
+    });
   }
 }
