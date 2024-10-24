8b657b483d8d38b22b404f24c4f4a2fc27094f83
林明虎 <hayashi20030703@gmail.com>
2024-10-24 22:37:37 +0900

    removed unnecessary instance

--- riverpod_practice/lib/model/external/pref_service.dart
+++ riverpod_practice/lib/model/external/pref_service.dart
@@ -1,8 +1,5 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-PrefService prefService_1 = PrefService();
-PrefService prefService_2 = PrefService();
-
 class PrefService {
   static final PrefService _instance = PrefService._internal();
   static SharedPreferences? _pref;
